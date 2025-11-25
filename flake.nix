0008d866918109c57fb1a2b724de44c4d796e010
Boqer <athrun.judah@proton.me>
2025-11-25 16:14:23 -0500

    fixed: flake.nix syntax error

--- flake.nix
+++ flake.nix
@@ -44,7 +44,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = {inherit inputs;}
+            home-manager.extraSpecialArgs = {inherit inputs;};
             home-manager.users.${username} = import ./home/default.nix;
           }
         ];
